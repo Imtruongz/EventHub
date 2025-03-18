029afac07900d65664d553b16ff65503ea02569a
truong.nv <truong16042003@gmail.com>
2025-03-18 16:55:22 +0700

    first commit

---
+++ src/screens/events/SearchEvents.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const SearchEvents = () => {
+  return (
+    <View>
+      <Text>SearchEvents</Text>
+    </View>
+  )
+}
+
+export default SearchEvents
+
+const styles = StyleSheet.create({})
---
+++ src/screens/index.ts
@@ -0,0 +1,25 @@
+import SplashScreen from './SplashScreen';
+import LoginScreen from './auth/LoginScreen';
+import HomeScreen from './home/HomeScreen';
+import ForgotPassword from './auth/ForgotPassword';
+import AddNewScreen from './AddNewScreen';
+import SignUpScreen from './auth/SignUpScreen';
+import Verification from './auth/Verification';
+import EventsScreen from './events/EventsScreen';
+import SearchEvents from './events/SearchEvents';
+import MapScreen from './maps/MapScreen';
+import ProfileScreen from './profiles/ProfileScreen';
+
+export {
+  HomeScreen,
+  LoginScreen,
+  SplashScreen,
+  Verification,
+  ForgotPassword,
+  SignUpScreen,
+  AddNewScreen,
+  EventsScreen,
+  MapScreen,
+  ProfileScreen,
+  SearchEvents,
+};
